0a20376f3d52c1e3e8f3cac2f2a8a405c485ac44
pasyaraffi <raffirivanja@gmail.com>
2025-08-20 17:14:53 +0700

    UPDATE2

--- resources/views/pages/produk/show.blade.php
+++ resources/views/pages/produk/show.blade.php
@@ -3,28 +3,33 @@
 <h1>Daftar Produk</h1>
 <hr>
 <a href="/product/create" type="button" class="btn btn-primary mb-3">Tambah Data</a>
-<!--Menampilkan tombol biru dengan teks "Tambah Data", yang mengarah ke URL /product/tambah. 
- Kelas Bootstrap btn btn-primary memberikan gaya tombol, dan mb-3 memberi margin bawah.-->
 <div class="alert alert-primary"> 
 <b>Nama Toko : </b>{{ $data_toko['nama_toko'] }}
-<!-- Menampilkan teks "Nama Toko:" dan nilainya dari variabel $nama_toko yang dikirim dari controller. -->
   <br>
 <b>Alamat : </b>{{ $data_toko['alamat'] }}
-<!-- Menampilkan teks "Alamat:" dan nilainya dari variabel $alamat.-->
   <br>
   <b>Tipe Toko : </b>{{ $data_toko['type'] }}
-<!-- Menampilkan teks "Tipe Toko:" dan nilainya dari variabel $type.-->
 </div>
 @if (session('message'))
-<div class="alert alert-primary">{{ session('message') }}</div>
+<div class="alert alert-primary">{{ session('message') }}</div> {{----}}
 @endif
+{{-- Kartu utama untuk daftar produk --}}
 <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">Daftar Produk
-      <form class="input-group" style="width: 350px;">
-        <input type="text" class="form-control" placeholder="Cari data produk">
-        <button class="btn btn-success" type="submit" id="button-addon2">Cari Data</button>
-      </form>
+      <div class="d-flex gap-2">
+         {{-- Jika ada keyword pencarian di URL, maka tampilkan tombol reset --}}
+        @if (Request()->keyword != '')
+        <a href="/product" class="btn btn-info">Reset</a>
+        @endif
+        {{-- Form pencarian produk --}}
+        <form class="input-group" style="width: 350px">
+          <input type="text" class="form-control" name="keyword" placeholder="Cari data produk">
+          <button class="btn btn-success" type="submit" id="button-addon2">Cari Data</button>
+        </form>
+      </div>
     </div>
+    
+
   <div class="card-body">
     <table class="table table-striped table-bordered">
   <thead>
@@ -38,28 +43,35 @@
   </thead>
   <tbody>
   {{-- Melakukan perulangan untuk setiap data produk yang dikirim dari controller --}}
-    @foreach ($data_produk as $item )
+    @forelse ($data_produk as $item )
       <tr>
         <th scope="row">{{ $loop->iteration }}</th> {{-- Menampilkan nomor urut --}}
         <td>{{ $item->nama_produk }}</td>  {{-- Menampilkan nama produk --}}
         <td>{{ $item->harga }}</td> {{-- Menampilkan harga produk --}}
-        <td>{{ $item->deskripsi }}</td> {{-- Menampilkan deskripsi produk --}}
+        <td>{{ $item->deskripsi_produk }}</td> {{-- Menampilkan deskripsi produk --}}
         <td>
+          <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#hapus{{ $item->id_produk }}">
+            Hapus
+          </button>
 
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#hapus{{ $item->id_produk }}">
-          {{-- Tombol untuk menghapus produk, akan memicu modal konfirmasi hapus --}}
-          Hapus
-        </button>
-          <!-- <button type="button" class="btn btn-danger">Hapus</button> -->
-          <a href="/product/{{ $item->id_produk }}/edit" class="btn btn-warning">Edit</button>
+            {{-- <button type="button" class="btn btn-danger">Hapus</button> --}}
+          <a href="/product/{{ $item->id_produk }}/edit" class="btn btn-warning">Edit</a>
           <a href="/product/{{ $item->id_produk }}" type="button" class="btn btn-info">Detail</a>
         </td>
       </tr>
-    @endforeach
+      @empty
+      <tr>
+        <td colspan="5" class="text-center">Data yang anda cari tidak ada!</td>
+      </tr>
+    @endforelse
+  
   </tbody>
 </table>
   </div>
 </div>
+
+
+<!-- Modal untuk konfirmasi hapus produk -->
 <!-- Modal -->
  @foreach ($data_produk as $item)
 <div class="modal fade" id="hapus{{ $item->id_produk }}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
